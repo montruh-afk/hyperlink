c728a226868ab647dd2e35648c945319a180692c
montruh-afk <charltonjohn668@gmail.com>
2026-01-24 15:52:42 +0000

    converting md to html works testing complete; works as expected

---
+++ src/htmlnode.py
@@ -0,0 +1,60 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+        
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+    
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+    
+    def __repr__(self):
+        return f'HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})'
+    
+    
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    
+        
+    def to_html(self):
+        if self.value is None or not self.value:
+            raise ValueError("All leaf nodes must have a value.")
+        elif self.tag is None or not self.tag:
+            return self.value
+        else:
+            return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+        
+    def __repr__(self):
+        return f'LeafNode({self.tag}, {self.value}, {self.props_to_html()})'
+    
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+        
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("no tag is present")
+        elif self.children is None:
+            raise ValueError("no children could be identified")
+        
+        formatted_string = ""
+        for child in self.children:
+            formatted_string += child.to_html()
+        
+        
+        return f'<{self.tag}{self.props_to_html()}>{formatted_string}</{self.tag}>'
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
